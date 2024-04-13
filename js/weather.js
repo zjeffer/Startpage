770012a832c9388b9e06bcf25f0beaf49bf6e681
Tuur Vanhoutte <4633209+zjeffer@users.noreply.github.com>
2024-04-13 14:07:02 +0200

    Update weather.js: fix lat and long
    I've been looking at the wrong weather for years :(

--- js/weather.js
+++ js/weather.js
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = 50.827969;
+    let longitude = 3.264930;
 
     getWeather(latitude, longitude);
 }
